4a5a1314183d71743f61a179f4d9a9f43702107a
KingRayuga <yashraj1605@gmail.com>
2023-12-20 20:35:33 +0530

    update: decoder to handle Byte[]

--- src/main/java/org/example/Bencoding_decoder.java
+++ src/main/java/org/example/Bencoding_decoder.java
@@ -3,42 +3,57 @@ package org.example;
 import java.util.*;
 
 public class Bencoding_decoder {
-    public static Object decode(String encodedString, int[] index){
-        if(null == encodedString || encodedString.isEmpty()){
+    public static Object decode(Byte[] encodedBytes, int[] index) {
+        if (encodedBytes == null || encodedBytes.length == 0) {
             return null;
         }
-        if(encodedString.charAt(index[0])=='i'){
-            int endIndex = encodedString.indexOf('e',index[0]);
-            int startIndex = index[0]+1;
-            index[0] = endIndex+1;
-            return Integer.parseInt(encodedString.substring(startIndex,endIndex));
-        }
-        else if(encodedString.charAt(index[0])=='l'){
+
+        if (encodedBytes[index[0]] == 'i') {
+            int endIndex = indexOf(encodedBytes, (byte) 'e', index[0]);
+            int startIndex = index[0] + 1;
+            index[0] = endIndex + 1;
+            Byte[] digitBytes = Arrays.copyOfRange(encodedBytes, startIndex, endIndex);
+            return bytesToInt(digitBytes);
+        } else if (encodedBytes[index[0]] == 'l') {
             List<Object> list = new ArrayList<>();
-            index[0]+=1;
-            while(encodedString.charAt(index[0])!='e'){
-                list.add(decode(encodedString,index));
+            index[0] += 1;
+            while (encodedBytes[index[0]] != 'e') {
+                list.add(decode(encodedBytes, index));
             }
             return list;
-        }
-        else if(encodedString.charAt(index[0])=='d'){
-            Map<Object,Object> dict = new TreeMap<>();
+        } else if (encodedBytes[index[0]] == 'd') {
+            Map<Object, Object> dict = new HashMap<>();
             index[0] += 1;
-            while(encodedString.charAt(index[0])!='e'){
-                Object key = decode(encodedString,index);
-                Object value = decode(encodedString,index);
-                dict.put(key,value);
+            while (encodedBytes[index[0]] != 'e') {
+                Object key = decode(encodedBytes, index);
+                Object value = decode(encodedBytes, index);
+                dict.put(key, value);
             }
             return dict;
-        }
-        else if(Character.isDigit(encodedString.charAt(index[0]))){
-            int colonIndex = encodedString.indexOf(':',index[0]);
-            String digit = encodedString.substring(index[0],colonIndex);
-            int digitValue = Integer.parseInt(digit);
-            String result = encodedString.substring(colonIndex+1,colonIndex+digitValue+1);
-            index[0] = colonIndex+digitValue+1;
+        } else if (Character.isDigit((char) encodedBytes[index[0]].byteValue())) {
+            int colonIndex = indexOf(encodedBytes, (byte) ':', index[0]);
+            int digitValue = bytesToInt(Arrays.copyOfRange(encodedBytes, index[0], colonIndex));
+            Byte[] result = Arrays.copyOfRange(encodedBytes, colonIndex + 1, colonIndex + digitValue + 1);
+            index[0] = colonIndex + digitValue + 1;
             return result;
         }
         return null;
+    }
+
+    private static int indexOf(Byte[] array, byte target, int fromIndex) {
+        for (int i = fromIndex; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private static int bytesToInt(Byte[] bytes) {
+        int result = 0;
+        for (Byte b : bytes) {
+            result = result * 10 + (b - '0');
+        }
+        return result;
     }
 }
