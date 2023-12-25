1a728a97b86f112a1acad6dbfa22e4b431b37fb0
KingRayuga <yashraj1605@gmail.com>
2023-12-25 13:04:57 +0530

    add: bitTorrent peer hand-shake

---
+++ src/main/java/org/example/BitTorrentHandShake.java
@@ -0,0 +1,79 @@
+package org.example;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Arrays;
+
+public class BitTorrentHandShake {
+    private final String peerIp;
+    private final int peerPort;
+    private final byte[] infoHash;
+    private final byte[] peerId;
+
+    public BitTorrentHandShake(String peerIp, int peerPort, byte[] infoHash, byte[] peerId) {
+        this.peerId = peerId;
+        this.peerPort = peerPort;
+        this.infoHash = infoHash;
+        this.peerIp = peerIp;
+    }
+
+    public void doHandShake() {
+        try {
+            Socket socket = new Socket(peerIp, peerPort);
+
+            if (doHandShake(socket)) {
+                System.out.println("Hand shake successful");
+            } else {
+                System.out.println("Hand shake failed");
+            }
+
+            socket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private boolean doHandShake(Socket socket) throws IOException {
+        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+        String protocolIdentifier = "BitTorrent Protocol";
+        byte[] protocolIdentifierBytes = protocolIdentifier.getBytes();
+
+        byte[] reservedBytes = new byte[8];
+
+        byte[] handShakeMessage = new byte[68];
+
+        System.arraycopy(protocolIdentifierBytes, 0, handShakeMessage, 0, 19);
+        System.arraycopy(reservedBytes, 0, handShakeMessage, 19, 8);
+        System.arraycopy(infoHash, 0, handShakeMessage, 27, 20);
+        System.arraycopy(peerId, 0, handShakeMessage, 47, 20);
+
+        dataOutputStream.write(handShakeMessage);
+
+        byte[] response = new byte[68];
+        dataInputStream.readFully(response);
+
+        return verifyHandShakeResponse(response);
+
+    }
+
+    private boolean verifyHandShakeResponse(byte[] response) {
+        byte[] recievedInfoHash = new byte[20];
+        System.arraycopy(response, 28, recievedInfoHash, 0, 20);
+
+        boolean infoHashMatch = Arrays.equals(recievedInfoHash, infoHash);
+
+        if (!infoHashMatch) {
+            System.out.println("infoHash mismatch");
+            return false;
+        }
+
+        return true;
+
+    }
+}
